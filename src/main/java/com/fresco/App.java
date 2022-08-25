6d193ee059f92378be8b4af3c00d89c008e3eb4b
Jairo Grateron <jgrateron@jgmail.com>
2022-08-25 18:39:48 -0400

    fix package

--- src/main/java/com/fresco/App.java
+++ src/main/java/com/fresco/App.java
@@ -1,4 +1,4 @@
-package fractales;
+package com.fresco;
 
 import java.awt.BorderLayout;
 import java.awt.event.WindowAdapter;
--- src/main/java/com/fresco/PanelFractalSierpinski.java
+++ src/main/java/com/fresco/PanelFractalSierpinski.java
@@ -1,4 +1,4 @@
-package fractales;
+package com.fresco;
 
 import java.awt.Color;
 import java.awt.Font;
